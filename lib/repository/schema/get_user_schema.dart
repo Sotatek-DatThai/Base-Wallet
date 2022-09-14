2c1056d755c8153be6d7de4c4d802fd3c55777de
M.Dat <dat.thai@sotatek.com>
2022-09-14 10:52:17 +0700

    add schema

---
+++ lib/repository/schema/get_user_schema.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'get_user_schema.g.dart';
+
+@JsonSerializable()
+class GetUserSchema {
+  final String id;
+
+  GetUserSchema(this.id);
+
+  factory GetUserSchema.fromJson(Map<String, dynamic> json) => _$GetUserSchemaFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GetUserSchemaToJson(this);
+}
---
+++ lib/repository/schema/get_user_schema.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_user_schema.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+GetUserSchema _$GetUserSchemaFromJson(Map<String, dynamic> json) =>
+    GetUserSchema(
+      json['id'] as String,
+    );
+
+Map<String, dynamic> _$GetUserSchemaToJson(GetUserSchema instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+    };
