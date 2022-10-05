705122c0ff98936b1ea577d8d7457d7be9a80c92
M.Dat <dat.thai@sotatek.com>
2022-10-05 10:51:17 +0700

    cannot write test for isar

--- lib/datasource/local/isar/isar_provider.dart
+++ lib/datasource/local/isar/isar_provider.dart
@@ -1,6 +1,13 @@
+import 'package:base_wallet/datasource/local/isar/models/ens_isar/ens_isar.dart';
+import 'package:base_wallet/datasource/local/isar/models/explorers_isar/explorers_isar.dart';
+import 'package:base_wallet/datasource/local/isar/models/native_currency/native_currency_isar.dart';
 import 'package:base_wallet/datasource/local/isar/models/network/network_isar.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:isar/isar.dart';
 
-final networkIsarPod =
-    FutureProvider<Isar>((ref) => Isar.open([NetworkIsarSchema]));
+final networkIsarPod = FutureProvider<Isar>((ref) => Isar.open([
+      NetworkIsarSchema,
+      NativeCurrencyIsarSchema,
+      EnsIsarSchema,
+      ExplorersIsarSchema,
+    ]));
--- test/datasource/local/isar/network/network_isar_datasource.dart
+++ test/datasource/local/isar/network/network_isar_datasource.dart
@@ -1,9 +1,24 @@
+import 'package:base_wallet/datasource/local/isar/models/ens_isar/ens_isar.dart';
+import 'package:base_wallet/datasource/local/isar/models/explorers_isar/explorers_isar.dart';
+import 'package:base_wallet/datasource/local/isar/models/native_currency/native_currency_isar.dart';
+import 'package:base_wallet/datasource/local/isar/models/network/network_isar.dart';
+import 'package:base_wallet/datasource/local/isar/network/network_isar_datasource.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:isar/isar.dart';
 
 void main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
 
+  late NetworkIsarDataSource networkIsarDataSource;
 
-  test('Expect network to be import from json', () {
-
+  setUpAll(() async {
+    networkIsarDataSource = NetworkIsarDataSource(await Isar.open([
+      NetworkIsarSchema,
+      NativeCurrencyIsarSchema,
+      EnsIsarSchema,
+      ExplorersIsarSchema,
+    ]));
   });
+
+  test('Expect network to be import from json', () async {});
 }
