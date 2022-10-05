88a39402501edd14b5de0861ab301cc7ff5b080e
M.Dat <dat.thai@sotatek.com>
2022-10-05 14:28:29 +0700

    done network isar datasource

--- lib/datasource/local/isar/network/network_isar_datasource.dart
+++ lib/datasource/local/isar/network/network_isar_datasource.dart
@@ -9,4 +9,25 @@ class NetworkIsarDataSource {
   Future<List<int>> saveNetworks(List<NetworkIsar> networks) async {
     return _isar.networkIsars.putAll(networks);
   }
+
+  Future<List<NetworkIsar>> getAll() async {
+    return _isar.networkIsars.where().findAll();
+  }
+
+  Future<NetworkIsar?> getAt(int chainId) {
+    return _isar.networkIsars.get(chainId);
+  }
+
+  Future<int> deleteAll() async {
+    return await _isar.writeTxn(() async {
+      return _isar.networkIsars
+          .deleteAll((await getAll()).map((e) => e.chainId).toList());
+    });
+  }
+
+  Future<bool> deleteAt(int chainId) async {
+    return await _isar.writeTxn(() async {
+      return _isar.networkIsars.delete(chainId);
+    });
+  }
 }
